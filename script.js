7cf2d168c79d7737f0101badc35275df21a9ee4c
fatmakus <124739045+fatmakus@users.noreply.github.com>
2025-02-12 14:52:16 +0300

    odev3

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
 async function fetchProducts() {
     const response = await fetch('https://fakestoreapi.com/products');
     const products = await response.json();
-<<<<<<< HEAD
     const container = document.getElementById('product-container');
     const modal = document.getElementById('product-modal');
     const closeModal = document.querySelector('.close');
-=======
-    
-    const container = document.getElementById('product-container');
-    const modal = document.getElementById('product-modal');
-    const closeModal = document.querySelector('.close');
-
->>>>>>> 4766b89d2511e8fa66fa7db120394aa9f935e5e8
     const modalImage = document.getElementById('modal-image');
     const modalTitle = document.getElementById('modal-title');
     const modalStars = document.getElementById('modal-stars');
     const modalCount = document.getElementById('modal-count');
     const modalPrice = document.getElementById('modal-price');
     const modalDescription = document.getElementById('modal-description');
-<<<<<<< HEAD
     closeModal.addEventListener('click', () => {
         modal.style.display = 'none';
     });
-=======
-
     closeModal.addEventListener('click', () => {
         modal.style.display = 'none';
     });
-
->>>>>>> 4766b89d2511e8fa66fa7db120394aa9f935e5e8
     products.slice(0, 10).forEach(product => {
         const card = document.createElement('div');
         card.className = 'card';
@@ -43,33 +30,19 @@ async function fetchProducts() {
                 <span class="count">(${product.rating.count})</span>
             </div>
             <p class="price">${product.price}$</p>
-<<<<<<< HEAD
             <button class="btn">Sepete Ekle</button>`;
-=======
-            <button class="btn">Sepete Ekle</button>
-        `;
-
->>>>>>> 4766b89d2511e8fa66fa7db120394aa9f935e5e8
         card.addEventListener('click', (event) => {
             if (!event.target.classList.contains('btn')) {
                 modalImage.src = product.image;
                 modalTitle.textContent = product.title;
-                modalStars.innerHTML = `${'★'.repeat(Math.round(product.rating.rate))}${'☆'.repeat(5 - Math.round(product.rating.rate))}`;
-                modalCount.textContent = `(${product.rating.count})`;
-                modalPrice.textContent = `${product.price}$`;
+                modalStars.innerHTML = '★'.repeat(Math.round(product.rating.rate)) +'☆'.repeat(5 - Math.round(product.rating.rate));
+                modalCount.textContent = product.rating.count;
+                modalPrice.textContent = product.price;
                 modalDescription.textContent = product.description;
                 modal.style.display = 'block';
             }
         });
-<<<<<<< HEAD
         container.appendChild(card);
     });
 }
-=======
-
-        container.appendChild(card);
-    });
-}
-
->>>>>>> 4766b89d2511e8fa66fa7db120394aa9f935e5e8
 fetchProducts();
